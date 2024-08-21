ead640e4e98963d1a9147a5a4474f57d7efc8362
The007Programmer <aahils2019@gmail.com>
2024-08-21 13:42:31 -0700

    update main.py;STABLE:class structure complete

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ class LuckySeat:
     def __init__(self):
         self.file = 'periods.json'
 
+        with open(self.file,'r') as jsonfile_init:
+            self.periods_dict = J.load(jsonfile_init)
+
+
     # Is _custom_builtin_ a good naming convention??
     def assert__(self, condition, error, func): #success = None#):
         '''
@@ -20,12 +24,12 @@ class LuckySeat:
             print(error)
             func()
 
-    def does_seat_exist(self):
+    def doesSeatExist(self):
         existing_seat_chart = input("\nUse seating chart of existing period? (y/n):    ").lower()
-        self.assert__(existing_seat_chart in ['y','n'], '\nPlease enter either `y` or `n`\n...Rerunning\n', self.does_seat_exist)
+        self.assert__(existing_seat_chart in ['y','n'], '\nPlease enter either `y` or `n`\n...Rerunning\n', self.main)
         return existing_seat_chart
 
-    def seat_exists(self):
+    def seatExists(self):
         '''
         DESC: If the user is using an existing seating chart, this will ask the period number and make sure that it is an integer between 1-7.
         ARGS: NONE
@@ -35,16 +39,23 @@ class LuckySeat:
             period_number = int(input("\nPeriod # (number input):    "))
             
             self.period_number = period_number
-            self.read_m()
+            with open(self.file, 'r') as jsonfile:
+                self.periods_dict = J.load(jsonfile)
 
             self.tables = self.periods_dict[self.period_number-1]["tables"]
             self.per_table = self.periods_dict[self.period_number-1]["per_table"]
+
         except ValueError:
             print('\nPlease make sure your value is a number!\n...Rerunning')
-            self.seat_exists()
-        self.assert__(self.period_number in [1,2,3,4,5,6,7], '\nPlease make sure your period number is between 1 to 7!`\n...Rerunning\n', self.seat_exists)
+            self.seatExists()
 
-    def seat_no_exists(self):
+        except IndexError:
+            print(f"The specified period doesn't have the number of tables you entered!\n...Rerunning")
+            self.seatExists()
+
+        self.assert__(self.period_number in [1,2,3,4,5,6,7], '\nPlease make sure your period number is between 1 to 7!`\n...Rerunning\n', self.seatExists)
+
+    def seatNoExists(self):
         '''
         DESC: If the user is using an existing seating chart, this will ask the period number and make sure that it is an integer between 1-7.
         ARGS: NONE
@@ -55,13 +66,15 @@ class LuckySeat:
             self.tables = tables
             per_table = int(input("\nStudents per table? (number input):    "))
             self.per_table = per_table
+
         except ValueError:
             print('\nPlease make sure your value is a number!\n...Rerunning')
-            self.seat_no_exists()
-        # better use of the per_table
-        self.assert__(self.tables and self.per_table in [1,2,3,4,5,6,7], '\nPlease make sure your period number is between 1 to 7!`\n...Rerunning\n', self.seat_no_exists)
+            self.seatNoExists()
 
-    def randomly_select(self):
+        # better use of the per_table
+        self.assert__(self.tables and self.per_table in [1,2,3,4,5,6,7], '\nPlease make sure your period number is between 1 to 7!`\n...Rerunning\n', self.seatNoExists)
+
+    def randomlySelect(self):
         
         returnlist = []
         tick = 1
@@ -74,78 +87,46 @@ class LuckySeat:
         
         print(f"\n{returnlist}")
 
-
-    def read_m(self):
-        with open(self.file, 'r') as jsonfile:
-            self.periods_dict = J.load(jsonfile)
-
-    def write_m(self):
-        with open(self.file, 'w') as jsonfile2:
-            J.dump(self.periods_dict, jsonfile2)
-
-    # def save_config(self):
-
-    #     try:
-    #         ask_save_config = input("\nSave configuration? (y/n):    ").lower()
-
-    #         if ask_save_config == 'n':
-    #             exit()
-    #         else:
-    #             self.write_m()
-            
-    #         ask_period_num = int(input("\nPeriod number to assign to?    "))
-            
-            
-    #     except ValueError:
-    #         self.assert__(ask_save_config in ['y','n'] and (ask_period_num in [1,2,3,4,5,6,7]), '\nPlease enter either `y` or `n`\n...Rerunning\n', self.save_config)
-
-    def save_config(self):
+    def saveConfig(self):
         try:
-            ask_save_config = input("\nSave configuration? (y/n):    ").lower()
-
-            if ask_save_config not in ['y', 'n']:
-                print("Invalid input! Please enter `y` or `n`.")
-                self.save_config()  # Retry if invalid input
+            ask_save_config = input("\nSave table configuration? (y/n):    ").lower()
+            self.assert__(ask_save_config in ['y', 'n'], '\nPlease enter either `y` or `n`\n...Rerunning\n', self.saveConfig)
 
             if ask_save_config == 'n':
                 exit()
+            else:
+                ask_period_num = int(input("\nPeriod number to assign to?    "))
+                self.assert__(1 <= ask_period_num <= 7, '\nPeriod number must be between 1 and 7.\n...Rerunning\n', self.saveConfig)
 
-            # Read period number and validate
-            ask_period_num = input("\nPeriod number to assign to?    ")
+                # Update the periods_dict with the new configuration
+                self.periods_dict[ask_period_num - 1] = {
+                    "tables": self.tables,
+                    "per_table": self.per_table
+                }
 
-            if not ask_period_num.isdigit():
-                print("Please make sure your input is a number!")
-                self.save_config()  # Retry if invalid input
-
-            ask_period_num = int(ask_period_num)
-
-            if ask_period_num not in [1, 2, 3, 4, 5, 6, 7]:
-                print("Invalid period number! It should be between 1 and 7.")
-                self.save_config()  # Retry if invalid input
-
-            # If all inputs are valid, proceed with saving
-            self.periods_dict[ask_period_num - 1] = {"tables": self.tables, "per_table": self.per_table}
-            self.write_m()
-            print(f"Configuration saved for period {ask_period_num}.")
+                # Save the updated dictionary back to the file
+                with open(self.file, 'w') as jsonfile2:
+                    J.dump(self.periods_dict, jsonfile2, indent=4)  # indent for readability
+                print(f"\nConfiguration saved for period {ask_period_num}.")
 
         except ValueError:
-            print("Invalid input! Please make sure your input is a number.")
-            self.save_config()  # Retry if invalid input
+            print("\nPlease enter a valid number.\n...Rerunning\n")
+            self.saveConfig()
 
 
         
     def main(self):
 
-        if self.does_seat_exist() == 'y':
+        if self.doesSeatExist() == 'y':
 
-            self.seat_exists()
-            self.randomly_select()
+            self.seatExists()
+            self.randomlySelect()
             
         else:
             
-            self.seat_no_exists()
-            self.randomly_select()
-            self.save_config()
+            self.seatNoExists()
+            self.randomlySelect()
+            self.saveConfig()
 
 if __name__ == '__main__':
     lucky = LuckySeat()
